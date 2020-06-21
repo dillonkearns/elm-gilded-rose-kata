94ff12aa35d5298df7f0470baab72ef092ab2017
Dillon Kearns <dillon@dillonkearns.com>
2020-06-21 16:30:03 -0700

    Upgrade to Elm 0.19.1.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -1,4 +1,4 @@
-module GildedRose exposing (Item(Item), updateQuality)
+module GildedRose exposing (Item(..), updateQuality)
 
 
 type alias GildedRose =
@@ -17,14 +17,17 @@ updateQuality =
 updateQualityItem (Item name sellIn quality) =
     let
         quality_ =
-            if (name /= "Aged Brie" && name /= "Backstage passes to a TAFKAL80ETC concert") then
+            if name /= "Aged Brie" && name /= "Backstage passes to a TAFKAL80ETC concert" then
                 if quality > 0 then
                     if name /= "Sulfuras, Hand of Ragnaros" then
                         quality - 1
+
                     else
                         quality
+
                 else
                     quality
+
             else if quality < 50 then
                 quality
                     + 1
@@ -35,42 +38,55 @@ updateQualityItem (Item name sellIn quality) =
                                     + (if sellIn < 6 then
                                         if quality < 48 then
                                             1
+
                                         else
                                             0
+
                                        else
                                         0
                                       )
+
                             else
                                 0
+
                         else
                             0
+
                        else
                         0
                       )
+
             else
                 quality
 
         sellIn_ =
             if name /= "Sulfuras, Hand of Ragnaros" then
                 sellIn - 1
+
             else
                 sellIn
     in
-        if sellIn_ < 0 then
-            if name /= "Aged Brie" then
-                if name /= "Backstage passes to a TAFKAL80ETC concert" then
-                    if quality_ > 0 then
-                        if name /= "Sulfuras, Hand of Ragnaros" then
-                            (Item name sellIn_ (quality_ - 1))
-                        else
-                            (Item name sellIn_ quality_)
+    if sellIn_ < 0 then
+        if name /= "Aged Brie" then
+            if name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if quality_ > 0 then
+                    if name /= "Sulfuras, Hand of Ragnaros" then
+                        Item name sellIn_ (quality_ - 1)
+
                     else
-                        (Item name sellIn_ quality_)
+                        Item name sellIn_ quality_
+
                 else
-                    (Item name sellIn_ (quality_ - quality_))
-            else if quality_ < 50 then
-                (Item name sellIn_ (quality_ + 1))
+                    Item name sellIn_ quality_
+
             else
-                (Item name sellIn_ quality_)
+                Item name sellIn_ (quality_ - quality_)
+
+        else if quality_ < 50 then
+            Item name sellIn_ (quality_ + 1)
+
         else
-            (Item name sellIn_ quality_)
+            Item name sellIn_ quality_
+
+    else
+        Item name sellIn_ quality_
