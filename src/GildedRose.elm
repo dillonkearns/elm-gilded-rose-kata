87b5da8d5401cf92b07c55b41e836e24a804a070
Dillon Kearns <dillon@dillonkearns.com>
2020-11-12 13:20:43 -0800

    TCR.

--- src/GildedRose.elm
+++ src/GildedRose.elm
@@ -76,6 +76,9 @@ updateQualityItem (Item name sellIn quality) =
     if name == "Sulfuras, Hand of Ragnaros" then
         updateSulfuras (Item name sellIn quality)
 
+    else if name == "Backstage passes to a TAFKAL80ETC concert" then
+        result
+
     else
         result
 
@@ -87,3 +90,69 @@ updateSulfuras (Item name sellIn quality) =
 
     else
         Item name sellIn (quality + 1)
+
+
+updateBackstagePasses : Item -> Item
+updateBackstagePasses (Item name sellIn quality) =
+    let
+        quality_ =
+            if name /= "Aged Brie" && name /= "Backstage passes to a TAFKAL80ETC concert" then
+                if quality <= 0 then
+                    quality
+
+                else
+                    quality - 1
+
+            else if quality < 50 then
+                quality
+                    + 1
+                    + (if name == "Backstage passes to a TAFKAL80ETC concert" then
+                        if sellIn < 11 then
+                            if sellIn < 6 && quality < 48 then
+                                2
+
+                            else if quality < 49 then
+                                1
+
+                            else
+                                0
+
+                        else
+                            0
+
+                       else
+                        0
+                      )
+
+            else
+                quality
+
+        sellIn_ =
+            sellIn - 1
+
+        result =
+            if sellIn_ < 0 then
+                if name /= "Aged Brie" then
+                    if name == "Backstage passes to a TAFKAL80ETC concert" then
+                        Item name sellIn_ (quality_ - quality_)
+
+                    else if quality_ > 0 then
+                        Item name sellIn_ (quality_ - 1)
+
+                    else
+                        Item name sellIn_ quality_
+
+                else if quality_ < 50 then
+                    Item name sellIn_ (quality_ + 1)
+
+                else
+                    Item name sellIn_ quality_
+
+            else
+                Item name sellIn_ quality_
+    in
+    if name == "Sulfuras, Hand of Ragnaros" then
+        updateSulfuras (Item name sellIn quality)
+
+    else
+        result
